2a30520889164db3de057a93926832a218a0d977
Anton Savin <a.nivas@yandex.ru>
2024-05-30 22:08:13 +0400

    upd

---
+++ bin/gendiff.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+import { program } from 'commander';
+
+program
+	.version('0.0.1', '-V, --version', 'output the version number')
+	.description('Compares two configuration files and shows a difference.');
+
+program.parse();
