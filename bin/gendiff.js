880296d4671cf9a0309dad312184a0a1d372a83d
Anton Savin <a.nivas@yandex.ru>
2024-07-22 21:47:05 +0400

    upd help option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,6 @@ import { program } from 'commander';
 program
 	.version('0.0.1', '-V, --version', 'output the version number')
 	.description('Compares two configuration files and shows a difference.')
-	//.argument('<type>')
-	.option('-f, --format [type]', 'output format')
+	.helpOption('-h, --help', 'output usage information')
 
 program.parse();
