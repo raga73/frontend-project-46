9cd0d4dd8564a9e13b2222005e890ad2398dc021
Anton Savin <a.nivas@yandex.ru>
2024-11-06 23:15:32 +0400

    upd

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { program } from 'commander';
 import _ from 'lodash';
-import { fileParce } from '../utils/parcer.js';
+import fileParce from '../utils/parcer.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
@@ -11,26 +11,26 @@ program
   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format [type]', 'output format')
   .action((filepath1, filepath2) => {
-   const file1 = (fileParce(filepath1));
-   const file2 = (fileParce(filepath2));
-   const commonKeys = _.union(Object.keys(file1), Object.keys(file2)).sort();
-   const filesDifferences = commonKeys.map((key) => {
-    if (!Object.hasOwn(file1, key)) {
-      return ` + ${key}: ${file2[key]}`;
-    } if (!Object.hasOwn(file2, key)) {
-      return ` - ${key}: ${file1[key]}`;
-    }
+    const file1 = (fileParce(filepath1));
+    const file2 = (fileParce(filepath2));
+    const commonKeys = _.union(Object.keys(file1), Object.keys(file2)).sort();
+    const filesDifferences = commonKeys.map((key) => {
+      if (!Object.hasOwn(file1, key)) {
+        return ` + ${key}: ${file2[key]}`;
+      } if (!Object.hasOwn(file2, key)) {
+        return ` - ${key}: ${file1[key]}`;
+      }
       if (file1[key] !== file2[key]) {
-      return ` - ${key}: ${file1[key]}\n + ${key}: ${file2[key]}`;
-    }
+        return ` - ${key}: ${file1[key]}\n + ${key}: ${file2[key]}`;
+      }
       if (file1[key] === file2[key]) {
         return `   ${key}: ${file1[key]}`;
-    }
-  }
-);
-  const stringWithFilesDifferences = filesDifferences.join('\n');
-  console.log(`{\n${stringWithFilesDifferences}\n}`);
-}
-);
+      }
+      return null;
+    });
+    const stringWithFilesDifferences = filesDifferences.join('\n');
+    console.log(`{\n${stringWithFilesDifferences}\n}`);
+    return stringWithFilesDifferences;
+  });
 
 program.parse();
--- utils/parcer.js
+++ utils/parcer.js
@@ -1,6 +1,4 @@
-import path from "node:path";
-import fs from "node:fs";
+import path from 'node:path';
+import fs from 'node:fs';
 
-const fileParce = (filePath) => JSON.parse(fs.readFileSync(path.resolve(filePath)));
-
-export { fileParce };
+export default (filePath) => JSON.parse(fs.readFileSync(path.resolve(filePath)));
