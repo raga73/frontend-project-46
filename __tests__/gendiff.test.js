90b9aca137f58e350e95715e1974d0e67c215151
Anton Savin <a.nivas@yandex.ru>
2024-11-08 21:15:07 +0400

    upd

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -25,7 +25,7 @@ test('yaml plain gendiff check', () => {
     const file3 = getFixturePath('file3.yml');
     const file4 = getFixturePath('file4.yaml');
     const received = genDiff(file3, file4);
-    const expected = readFile('file3_file4_differences.txt').trimEnd();
+    const expected = readFile('file3_file4_differences.txt');
     console.log(received)
     console.log(expected)
     expect(received).toEqual(expected);
