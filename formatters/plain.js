ef835d95c7712ecd114ca86b1cef60392febc87e
Anton Savin <a.nivas@yandex.ru>
2024-11-20 00:51:39 +0400

    upd

--- formatters/plain.js
+++ formatters/plain.js
@@ -12,42 +12,27 @@ const plainFormatter = (obj) => {
         return value;
     };
   
-    const iter = (currentValue) => {
+    const iter = (currentValue, path = '') => {
+      path = path ?? '';
+      const currPath = path === '' ? '' : `${path}.`
       const line = Object
       .entries(currentValue)
       .map(([key, value]) => {  
         switch (value.mark) {
         case 'removed':
-          return `Property '${key}' was removed\n`;
+          return `Property '${currPath}${key}' was removed\n`;
         case 'added':
-          return `Property '${key}' was added with value: ${format(value.children)}\n`;
+          return `Property '${currPath}${key}' was added with value: ${format(value.children)}\n`;
         case 'updated':
-          return `Property '${key}' was updated. From ${format(value.children.old)} to ${format(value.children.new)}\n`;
+          return `Property '${currPath}${key}' was updated. From ${format(value.children.old)} to ${format(value.children.new)}\n`;
         case 'diff':
-          return `${iter(value.children)}`;
+          return `${iter(value.children, currPath + key)}`;
         }
       });
       return line.join('');
     };
 
- /* let ancestor = '';
-  const str = Object
-    .entries(obj)
-    .reduce((acc, [key, value]) => {
-      switch (value.mark) {
-        case 'diff':
-          acc += iter(value.children, ancestor = key);
-            return acc;
-        case 'removed':
-          acc += `Property '${key}' was removed\n`;
-            return acc;
-        case 'added':
-          acc += `Property '${key}' was added with value: ${format(value.children)}\n`;
-            return acc;
-      }
-    }, '');
-    return str;*/
-    return iter(obj)
+    return iter(obj);
 };
 
 export default plainFormatter;
