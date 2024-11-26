5a0991e8f034ea2085e2085f96eadcb9202f8d6c
Anton Savin <a.nivas@yandex.ru>
2024-11-26 21:40:21 +0400

    upd

--- utils/parser.js
+++ utils/parser.js
@@ -11,10 +11,10 @@ export default (filePath) => {
 
   const fileExtension = path.extname(getPath(filePath));
 
-  if (fileExtension === '.json') {
+  if (fileExtension === 'json') {
     return JSON.parse(fs.readFileSync(getPath(filePath)));
   }
-  if (fileExtension === '.yml' || fileExtension === '.yaml') {
+  if (fileExtension === 'yml' || fileExtension === 'yaml') {
     return yaml.load(fs.readFileSync(getPath(filePath)));
   }
   throw new Error(`Wrong file extension ${fileExtension}!`);
