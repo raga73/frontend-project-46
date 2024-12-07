68fd5db2fd9b1fcf81916697dd4f5da39a4cab2b
Anton Savin <a.nivas@yandex.ru>
2024-12-07 20:26:18 +0400

    upd

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -3,12 +3,12 @@ import yaml from 'js-yaml';
 
 export default (file, fileExtension) => {
   switch (fileExtension) {
-  case '.json':
-    return JSON.parse(fs.readFileSync(file));
-  case '.yml': 
-  case '.yaml': 
-    return yaml.load(fs.readFileSync(file));
-  default:  
-  throw new Error(`Wrong file extension ${fileExtension}!`);
+    case '.json':
+      return JSON.parse(fs.readFileSync(file));
+    case '.yml':
+    case '.yaml':
+      return yaml.load(fs.readFileSync(file));
+  default:
+      throw new Error(`Wrong file extension ${fileExtension}!`);
   }
 };
