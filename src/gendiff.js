eec19b3c6d34df9cee720a288d5e53e3995f0224
Anton Savin <a.nivas@yandex.ru>
2024-12-07 20:22:58 +0400

    upd

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,10 +1,13 @@
 import _ from 'lodash';
+import { cwd } from 'process';
+import path from 'node:path';
 import fileParse from './utils/parser.js';
 import formatter from './formatters/index.js';
 
 export default (filePath1, filePath2, formatType) => {
-  const file1 = (fileParse(filePath1));
-  const file2 = (fileParse(filePath2));
+  const getPath = (filename) => path.resolve(cwd(), filename);
+  const file1 = fileParse(getPath(filePath1), path.extname(filePath1));
+  const file2 = fileParse(getPath(filePath2), path.extname(filePath2));
 
   const iter = (node1, node2) => {
     const commonKeys = _.sortBy(_.union(Object.keys(node1), Object.keys(node2)));
@@ -47,7 +50,7 @@ export default (filePath1, filePath2, formatType) => {
           mark: 'added',
         };
       }
-      return '';
+      throw new Error('Something wrong with file content!');
     });
     return filesDifferences;
   };
--- src/utils/parser.js
+++ src/utils/parser.js
@@ -1,16 +1,14 @@
-import path from 'node:path';
 import fs from 'node:fs';
-import { cwd } from 'process';
 import yaml from 'js-yaml';
 
-export default (filePath) => {
-  const getPath = (filename) => path.resolve(cwd(), filename);
-  const fileExtension = path.extname(filePath);
-  if (fileExtension === '.json') {
-    return JSON.parse(fs.readFileSync(getPath(filePath)));
-  }
-  if (fileExtension === '.yml' || fileExtension === '.yaml') {
-    return yaml.load(fs.readFileSync(getPath(filePath)));
-  }
+export default (file, fileExtension) => {
+  switch (fileExtension) {
+  case '.json':
+    return JSON.parse(fs.readFileSync(file));
+  case '.yml': 
+  case '.yaml': 
+    return yaml.load(fs.readFileSync(file));
+  default:  
   throw new Error(`Wrong file extension ${fileExtension}!`);
+  }
 };
